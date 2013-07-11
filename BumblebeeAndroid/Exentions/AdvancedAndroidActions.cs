32dc6e5f00386c8d03a069fb6ef17e3c7ffe0e89
Nico Higuera <nicohiguera11@gmail.com>
2013-07-11 11:12:44 -0700

    added ClickAtLocation functionality

---
+++ BumblebeeAndroid/Exentions/AdvancedAndroidActions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bumblebee.Interfaces;
+using OpenQA.Selenium.Interactions;
+
+namespace BumblebeeAndroid.Exentions
+{
+    public static class AdvancedAndroidActions
+    {
+        public static TResult ClickAtLocation<TResult>(this IBlock block, int x, int y) where TResult : IBlock
+        {
+            new TouchActions(block.Session.Driver).Down(x, y).Up(x, y).Perform();
+            return block.Session.CurrentBlock<TResult>();
+        }
+    }
+}
